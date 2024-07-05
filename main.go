932a76e467e1122a48e4ccfe8a522688ca99df16
yyaskriloff <69407772+yyaskriloff@users.noreply.github.com>
2024-07-05 16:38:28 +0300

    add go routines to compress file

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"log"
+	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
 )
 
 func main() {
@@ -36,4 +39,44 @@ func main() {
 
 	fmt.Println("File Path:", *filePath)
 
+	output480p := strings.TrimSuffix(*filePath, ext) + "_480p" + ext
+	output720p := strings.TrimSuffix(*filePath, ext) + "_720p" + ext
+	outputAudio := strings.TrimSuffix(*filePath, ext) + "_audio.mp3"
+
+	var wg sync.WaitGroup
+
+	wg.Add(3)
+
+	go func() {
+		defer wg.Done()
+		runFFmpegCommand(*filePath, "-vf", "scale=-2:480", "-c:a", "copy", output480p)
+		fmt.Println("Compressed to 480p saved to:", output480p)
+	}()
+
+	go func() {
+		defer wg.Done()
+		runFFmpegCommand(*filePath, "-vf", "scale=-2:720", "-c:a", "copy", output720p)
+		fmt.Println("Compressed to 720p saved to:", output720p)
+
+	}()
+
+	go func() {
+		defer wg.Done()
+		runFFmpegCommand(*filePath, "-vn", "-c:a", "libmp3lame", "-q:a", "4", outputAudio)
+		fmt.Println("Audio extracted and saved to:", outputAudio)
+	}()
+
+}
+
+func runFFmpegCommand(inputFile string, args ...string) {
+	cmdArgs := append([]string{"-i", inputFile}, args...)
+	cmd := exec.Command("ffmpeg", cmdArgs...)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Command failed: %v\nOutput: %s\nError: %s", err, out.String(), stderr.String())
+	}
 }
